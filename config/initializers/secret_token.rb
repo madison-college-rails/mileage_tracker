0de1c716fbc1ff3387daa445f0da5ad25e3b6744
bendycode <stephen@bendyworks.com>
2011-03-08 19:06:51 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MileageTracker::Application.config.secret_token = 'f803acf87818640b1cc31300dbe818ddd556674aed9c9ec7c97dcd896e9fffa1cece5199807e64f9c16f41b50773ab46c9acd58b8a96ef2af0324ba2da987c6f'
