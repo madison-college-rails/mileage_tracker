a7e3aca1d1f6fd7a0539000e778cb861c7ab1746
bendycode <stephen@bendyworks.com>
2011-03-29 20:33:50 -0500

    Model drop down on new vehicle page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+[Make, Model, Vehicle].each(&:delete_all)
+
+ford = Make.create! :name => 'Ford'
+toyota = Make.create! :name => 'Toyota'
+
+mustang = Model.create! :make => ford, :name => 'Mustang'
+Model.create! :make => ford, :name => 'Focus'
+Model.create! :make => toyota, :name => 'Prius'
+camry = Model.create! :make => toyota, :name => 'Camry'
+
+Vehicle.create! :model => mustang, :color => 'Black', :year => 1965,
+  :license_plate_number => 'Z00M'
+
+Vehicle.create! :model => camry, :color => 'Blue', :year => 2005,
+  :license_plate_number => 'MATC1'
