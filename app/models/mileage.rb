28b53cc95c7e31c4b23364e97099104e4262b228
bendycode <stephen@bendyworks.com>
2011-04-18 21:44:43 -0500

    Mileage validation

--- app/models/mileage.rb
+++ app/models/mileage.rb
@@ -1,3 +1,7 @@
 class Mileage < ActiveRecord::Base
   belongs_to :vehicle
+  validates_presence_of :vehicle_id
+  validates_presence_of :filled_tank_at
+  validates_presence_of :miles
+  validates_presence_of :gallons
 end
--- spec/models/mileage_spec.rb
+++ spec/models/mileage_spec.rb
@@ -6,4 +6,9 @@ describe Mileage do
   it { should have_db_column(:filled_tank_at).of_type(:time) }
   it { should have_db_column(:miles).of_type(:decimal).with_options(:precision => 10, :scale => 2) }
   it { should have_db_column(:gallons).of_type(:decimal).with_options(:precision => 10, :scale => 2) }
+
+  it { should validate_presence_of(:vehicle_id) }
+  it { should validate_presence_of(:filled_tank_at) }
+  it { should validate_presence_of(:miles) }
+  it { should validate_presence_of(:gallons) }
 end
