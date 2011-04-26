525a7ec095eefe8844598dc61959365cbd0b3689
Chris Arndt <chrisgo.0075@gmail.com>
2011-04-26 18:59:47 -0500

    finished lab step 3

--- app/controllers/mileages_controller.rb
+++ app/controllers/mileages_controller.rb
@@ -7,14 +7,15 @@ class MileagesController < ApplicationController
   end
 
   def create
+    puts "passed params are: #{params}"
     filled_tank_on_year = params[:filled_tank_on_year]
     filled_tank_on_month = params[:filled_tank_on_month]
     filled_tank_on_day = params[:filled_tank_on_day]
     license_plate_number = params[:vehicle_id]
 
     mileage = Mileage.new
-
-    mileage.vehicle = Vehicle.find_by_license_plate_number(license_plate_number)
+    # mileage.vehicle = Vehicle.find_by_license_plate_number(license_plate_number)
+    mileage.vehicle = Vehicle.where(:license_plate_number => license_plate_number).first
     mileage.miles = params[:miles]
     mileage.gallons = params[:gallons]
     mileage.filled_tank_on = "#{filled_tank_on_year}-#{filled_tank_on_month}-#{filled_tank_on_day}" 
