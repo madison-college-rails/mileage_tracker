cc8fd9022aa087f49f4fe1cc2122c7403817847b
Chris Arndt <chrisgo.0075@gmail.com>
2011-04-26 19:55:41 -0500

    finished lab step 5

--- app/controllers/mileages_controller.rb
+++ app/controllers/mileages_controller.rb
@@ -7,26 +7,18 @@ class MileagesController < ApplicationController
   end
 
   def create
-    license_plate_number = params[:mileage][:vehicle_id]
-    vehicle = Vehicle.find_by_license_plate_number(license_plate_number)
-    params[:mileage][:vehicle_id] = vehicle.id
-    params[:mileage][:filled_tank_on] = build_date(params[:mileage][:filled_tank_on])
+  license_plate_number = params[:mileage][:vehicle_id]
+  vehicle = Vehicle.find_by_license_plate_number(license_plate_number)
+  params[:mileage][:vehicle_id] = vehicle.id
+  # params[:mileage][:filled_tank_on] = build_date(params[:mileage][:filled_tank_on])
 
-    mileage = Mileage.new(params[:mileage])
+  mileage = Mileage.new(params[:mileage])
 
-    if mileage.save
-      flash[:notice] = 'Mileage entry was successfully created'
-      redirect_to mileages_path
-    else
-      render :action => 'new'
-    end
+  if mileage.save
+    flash[:notice] = 'Mileage entry was successfully created'
+    redirect_to mileages_path
+  else
+    render :action => 'new'
   end
-
-   
-  private
-
-  def build_date date_hash
-    Date.new(date_hash[:year].to_i, date_hash[:month].to_i, date_hash[:day].to_i)
   end
-  
 end
