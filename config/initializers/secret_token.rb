475776a1ae31dd9c4cb17b7b856fe6fabf9c0e29
Mark McEahern <mark@mceahern.com>
2014-04-27 17:46:18 -0500

    rails new mileage_tracker --database postgresql --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MileageTracker::Application.config.secret_key_base = '649d8e513498635787c3d7adf6bccf2bb06e04ea79d8386dac412d9102d4550b18dc03a9fab60d7c554da830ff5bb5b69f5c6fc956b31771d256fe22fafb8376'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MileageTracker::Application.config.session_store :cookie_store, key: '_mileage_tracker_session'
